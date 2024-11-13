91fb50f19b7f4c3bb7853c9915bed2d326d0b7bc
EC2 Default User <ec2-user@ip-172-31-21-11.ap-northeast-1.compute.internal>
2024-11-13 11:20:01 +0000

    [Add] 実装フェーズ1

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         attr_accessor :username
+         attribute :name, :string
+        #  has_many :posts, dependent: :destroy
+          has_many :posts, dependent: :destroy
+        
+         has_one_attached :profile_image
+         validates :name, uniqueness: true, length: { in: 2..20 }
+         validates :email, presence: true
+        #  validates :password, presence: true
+         validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+
+          # パスワードの長さのバリデーション（deviseが既に設定しているがカスタマイズしたい場合）
+         validates :password, length: { minimum: 6 }, allow_nil: true
+
+def get_profile_image(width, height)
+  unless profile_image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+  profile_image.variant(resize_to_limit: [width, height]).processed
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+ root to: "homes#top"
+ get 'mypage/:id', to: 'users#mypage', as: 'mypage'
+ resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+ resources :users, only: [ :show, :edit, :update, :destroy]
+ resources :books, only: [:index, :show]
+ get "/homes/about" => "homes#about", as: "about"
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
